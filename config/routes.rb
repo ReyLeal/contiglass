cb384612655d78a6a70906b51d21bae41c5f05a3
xrey007 <reinaldo.leal001@gmail.com>
2017-04-05 13:32:46 -0400

    addes missing files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'glass_floors/index'
+
+  get 'metal_railings/index'
+
+  get 'metal_fabrications/index'
+
+  get 'glass_stairs/index'
+
+  get 'glass_railings/index'
+
+  get 'glass_elevators/index'
+
+  get 'all_photos/index'
+
+  get 'glass_wine_rooms/index'
+
+  get 'glass_windows_doors/index'
+
+  get 'glass_shower_doors/index'
+
+  get 'other_glass_structures/index'
+
+  resources :photos
+  root 'photos#homepage'
 end
